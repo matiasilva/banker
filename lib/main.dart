ca5f2859d16898f6645bcf89cd9e974dd215fd4f
Matias Silva <git@matiasilva.com>
2022-08-30 20:05:09 +0100

    Update models and display data

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,118 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+import 'package:intl/intl.dart';
+import 'models/transaction.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  String databasePath = join(await getDatabasesPath(), 'db.sqlite3');
-  final database = openDatabase(databasePath, onCreate: (db, version) {
-    return db.execute(
-      'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
-    );
-  }, version: 1);
+  String databasePath = await Transaction.getDefaultPath();
+  File db = File(databasePath);
+  if (db.existsSync()) {
+    db.deleteSync(); // blow the DB away
+  }
+  TransactionProvider provider = await TransactionProvider().open();
+  for (var i = 0; i < 5; i++) {
+    provider.insert(Transaction(
+        merchant: 'dummy merchant',
+        value: 5,
+        datePurchased: DateTime.now().toIso8601String(),
+        category: 'lel'));
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static const String _title = 'Banker';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: _title,
+      theme:
+          ThemeData(primarySwatch: Colors.grey, backgroundColor: Colors.white),
+      home: const MyHomePage(title: 'Banker'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Future<List<Transaction>> getTransactions() async {
+    TransactionProvider provider = await TransactionProvider().open();
+    return provider.getTransactions();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<DataRow> buildDataRows(List<Transaction>? data) {
+    if (data == null) {
+      throw ('Data returned from DB was null!');
+    }
+    return List<DataRow>.generate(data.length, (i) => buildDataRow(data[i], i));
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  DataRow buildDataRow(Transaction trans, int i) {
+    List<String> cells = [
+      trans.merchant,
+      (trans.isDebit ? 1 : 0).toString(),
+      trans.value.toString(),
+      DateFormat.yMMMd('en_US').format(DateTime.parse(trans.datePurchased)),
+      trans.category,
+      trans.note
+    ];
+    return DataRow(
+        color: MaterialStateProperty.resolveWith<Color?>(
+            (Set<MaterialState> states) {
+          print(states);
+          // Even rows will have a grey color.
+          if (i.isEven) {
+            return Colors.grey.withOpacity(0.3);
+          }
+          return null; // Use default value for other states and odd rows.
+        }),
+        cells: cells.map((e) => DataCell(Text(e))).toList());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: FutureBuilder<List<Transaction>>(
+            future: getTransactions(),
+            builder: (BuildContext context,
+                AsyncSnapshot<List<Transaction>> snapshot) {
+              if (snapshot.hasData) {
+                return DataTable(
+                  columns: const <DataColumn>[
+                    DataColumn(label: Text('Merchant')),
+                    DataColumn(label: Text('Flow')),
+                    DataColumn(label: Text('Value (£)')),
+                    DataColumn(label: Text('Date')),
+                    DataColumn(label: Text('Category')),
+                    DataColumn(label: Text('Note')),
+                  ],
+                  rows: buildDataRows(snapshot.data),
+                );
+              } else if (snapshot.hasError) {
+                return const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                );
+              } else {
+                return const SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
   }
 }
--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,3 +1,4 @@
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 const String tableName = 'transactions';
@@ -7,37 +8,47 @@ const String columnisDebit = 'is_debit';
 const String columnValue = 'value';
 const String columnCurrency = 'currency';
 const String columnDatePurchased = 'date_purchased';
+const String columnCategory = 'category';
 const String columnNote = 'note';
 
 class Transaction {
-  int id;
+  int? id;
   String merchant;
   bool isDebit;
   int value;
   String currency;
   String datePurchased;
+  String category;
   String note;
 
   Transaction({
-    required this.id,
     required this.merchant,
     this.isDebit = true,
     required this.value,
     this.currency = 'GBP',
     required this.datePurchased,
+    required this.category,
     this.note = "",
   });
 
   Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'merchant': merchant,
-      'isDebit': isDebit,
-      'value': value,
-      'currency': currency,
-      'datePurchased': datePurchased,
-      'note': note,
+    var map = <String, dynamic>{
+      columnMerchant: merchant,
+      columnisDebit: isDebit ? 1 : 0,
+      columnValue: value,
+      columnCurrency: currency,
+      columnDatePurchased: datePurchased,
+      columnCategory: category,
+      columnNote: note,
     };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+  static Future<String> getDefaultPath() async {
+    return join(await getDatabasesPath(), 'db.sqlite3');
   }
 
   @override
@@ -51,15 +62,18 @@ class Transaction {
         isDebit = map[columnisDebit] == 1,
         value = map[columnValue],
         currency = map[columnCurrency],
+        category = map[columnCategory],
         datePurchased = map[columnDatePurchased],
         note = map[columnNote];
 }
 
-class TodoProvider {
-  late Database db;
+class TransactionProvider {
+  Database? db;
 
-  Future open(String path) async {
-    db = await openDatabase(path, version: 1,
+  Future<TransactionProvider> open({String? path}) async {
+    String databasePath = path ?? join(await getDatabasesPath(), 'db.sqlite3');
+    await Sqflite.setDebugModeOn();
+    db = await openDatabase(databasePath, version: 1, singleInstance: true,
         onCreate: (Database db, int version) async {
       await db.execute('''
         create table $tableName ( 
@@ -68,36 +82,65 @@ class TodoProvider {
           $columnisDebit integer not null,
           $columnValue integer not null,
           $columnCurrency text not null,
-          $columnDatePurchased text not null
+          $columnDatePurchased text not null,
+          $columnCategory text not null,
           $columnNote text not null
         )
       ''');
     });
+    return this;
   }
 
   Future<Transaction> insert(Transaction trans) async {
-    trans.id = await db.insert(tableName, trans.toMap());
+    if (db == null) {
+      throw ('Database object does not exist!');
+    }
+    trans.id = await db!.insert(tableName, trans.toMap());
     return trans;
   }
 
-  Future<List<Transaction>> getTransactions(int id) async {
-    List<Map<String, dynamic>> maps = await db.query(tableName,
-        columns: [columnId, columnMerchant, columnValue, columnDatePurchased],
-        where: '$columnId = ?',
-        whereArgs: [id]);
+  Future<Transaction?> getTransactionById(int id) async {
+    if (db == null) {
+      throw ('Database object does not exist!');
+    }
+    List<Map<String, dynamic>> maps = await db!.query(tableName,
+        columns: null, where: '$columnId = ?', whereArgs: [id]);
+    if (maps.isNotEmpty) {
+      return Transaction.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  Future<List<Transaction>> getTransactions() async {
+    if (db == null) {
+      throw ('Database object does not exist!');
+    }
+    List<Map<String, dynamic>> maps =
+        await db!.query(tableName, columns: null, where: null, limit: 5);
     return List.generate(maps.length, (i) {
       return Transaction.fromMap(maps[i]);
     });
   }
 
   Future<int> delete(int id) async {
-    return await db.delete(tableName, where: '$columnId = ?', whereArgs: [id]);
+    if (db == null) {
+      throw ('Database object does not exist!');
+    }
+    return await db!.delete(tableName, where: '$columnId = ?', whereArgs: [id]);
   }
 
   Future<int> update(Transaction trans) async {
-    return await db.update(tableName, trans.toMap(),
+    if (db == null) {
+      throw ('Database object does not exist!');
+    }
+    return await db!.update(tableName, trans.toMap(),
         where: '$columnId = ?', whereArgs: [trans.id]);
   }
 
-  Future close() async => db.close();
+  Future close() async {
+    if (db == null) {
+      throw ('Database object does not exist!');
+    }
+    db!.close();
+  }
 }
